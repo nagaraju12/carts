a4ffae64e323de6331b667ad73b4fc6155f3ffb9
asy <rails@railsnew.com>
2015-07-01 18:01:31 +0530

    naga

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  def is_login?
+    unless current_user
+      redirect_to root_path
+    end
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+  end
+  def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
+end
+def configure_permitted_parameters
+  devise_parameter_sanitizer.for(:sign_up) { |u| u.permit({ roles: [] }, :email, :password, :password_confirmation) }
+end
+private
+def set_cart
+@cart = Cart.find(session[:cart_id])
+rescue ActiveRecord::RecordNotFound
+@cart = Cart.create
+session[:cart_id] = @cart.id
+end
+end
